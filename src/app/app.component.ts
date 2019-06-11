bfb0e0379adf606f222af01b2c1ac99b1b9c4294
Adrienn Timko <adrienn.timko@gmail.com>
2019-06-11 15:03:57 +0200

    school project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExchangeService } from './exchange.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Exchange-Rates';
+
+  constructor(private exService: ExchangeService) {}
+
+
+
+  exchangeRate = '';
+  hasResult = false;
+  result = [];
+  currencies = ["CHF", "DKK", "EUR", "GBP", "HUF", "NOK", "SEK", "USD"];
+
+  getRates() {
+    this.exService.getExchangeRate(this.exchangeRate)
+    .subscribe((rate: any) => {
+      /*console.log(rate);*/
+      this.hasResult = true;
+      this.result = rate.rates;
+    });
+  }
 }
